da93bcd398cd5ccd459ca48e4be569a211f36bb1
root <root@ip-172-31-28-33.ap-south-1.compute.internal>
2018-10-25 07:55:50 +0000

    removing all files

--- client.rb
+++
@@ -1,4 +0,0 @@
-chef_server_url  "https://api.chef.io/organizations/bhaskar-sjb"
-validation_client_name "bhaskar-sjb-validator"
-log_location   STDOUT
-validation_key   "/etc/chef/bhaskar-sjb-validator.pem"
--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    shared_credentials_file = "${var.PATH_TO_CREDENTIAL}"
-    region = "${var.AWS_REGION}"
-
-}
